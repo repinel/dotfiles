88d90a54523f70cd427ad5348f76505201580fd7
Roque Pinel <1685896+repinel@users.noreply.github.com>
2022-02-01 15:40:38 -0500

    Fix homebrew paths

--- _fzf.zsh
+++ _fzf.zsh
@@ -1,13 +1,16 @@
 # Setup fzf
 # ---------
+
+FZF_PATH="/opt/homebrew/opt/fzf"
+
 if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+  export PATH="${PATH:+${PATH}:}${FZF_PATH}/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "${FZF_PATH}/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source "${FZF_PATH}/shell/key-bindings.zsh"
